e9850e573036b2121eaef7b67c06701631c8942f
RL-55 <rickyl55@nycstudents.net>
2025-11-26 09:16:40 -0500

    Update lc.py

--- lc.py
+++ lc.py
@@ -4,31 +4,44 @@ class Agent:
         self.name=name
         self.__hp=health
         self.__sp=sanity
-    def damage(self,effect):
-        self.__hp-=effect
-        print(f"{self.name} has take {effect} damage.")
-    def spdamage(self,effect):
-        self.__sp-=effect
-        print(f"{self.name} has take {effect} sp damage.")
+    def hpeffect(self,effect):
+        self.__hp+=effect
+        print(f"{self.name} has gained {effect} health.")
+    def speffect(self,effect):
+        self.__sp+=effect
+        print(f"{self.name} has gained {effect} sanity.")
     def status(self):
-        print(f"{self.name}'s health is{self.__hp} and sanity is{self.__sp}.")
+        print(f"{self.name}'s health is {self.__hp} and sanity is {self.__sp}.")
 name=input("Name your agent. ")
 health=random.randint(80,150)
-sanity=0
 Alive=True
-Nugget=Agent(name,health,sanity)
+Nugget=Agent(name,health,0)
 while Alive==True:
-    event=1
-    if event==1:
-        print(f"{name} has encounter a abnormality.")
-        action=(input("Fight or Flight ")).lower()
-        if action=="fight":
-            x=random.randint(1,150)
-            Agent.damage(Nugget,x)
-        elif action=="flight":
-            Nugget.spdamage(name,random.randint(1,150))
-        else:
-            print("Command not understood.")
-            Nugget.damage(name,80)
-            Nugget.spdamage(name,25)
+    if Agent(Nugget.__sp)<=-45:
+        print(f"{name} has distorted.")
+        rand=random.randint(100,200)
+        Agent.hpeffect(Nugget,rand)
+        Agent.speffect(Nugget,45)
+    if Agent.__hp<=0:
+        Alive=False
+        print(f"{name} has died.")
 
+    Stage=(input("Continue on or pause. ")).lower()
+    if Stage=="continue":
+        event=1
+        if event==1:
+            print(f"{name} has encounter a abnormality.")
+            action=(input("Fight or Flight ")).lower()
+            if action=="fight":
+                rand=random.randint(-1,-150)
+                Agent.hpeffect(Nugget,rand)
+            elif action=="flight":
+                rand=random.randint(-1,-45)
+                Agent.speffect(Nugget,rand)
+            else:
+                print("Command not understood.")
+                Agent.hpeffect(Nugget,-100)
+                Agent.speffect(Nugget,-25)
+    else:
+        print("You took a quick break.")
+        Agent.status(Nugget)
