02730f30c3797066021e5c6f16764d5f6661a97c
RL-55 <rickyl55@nycstudents.net>
2025-12-03 09:26:28 -0500

    Update lc.py

--- lc.py
+++ lc.py
@@ -1,95 +1,138 @@
 import random
-Alive=True
+i=1
 class Agent:
-    def __init__(self,name,health,sanity):
+    def __init__(self,name,health,sanity,enkephalin):
         self.name=name
+        self.__ek=enkephalin
         self.__hp=health
         self.__sp=sanity
     def hpeffect(self,effect):
         self.__hp+=effect
-        print(f"{self.name} has {effect} health.")
+        print(f"{self.name} inflicted {effect} health.")
     def speffect(self,effect):
         self.__sp+=effect
         if self.__sp>45:
             self.__sp=45
-        print(f"{self.name} has {effect} sanity.")  
+        print(f"{self.name} inflicted {effect} sanity.")  
+    def ekeffect(self,effect):
+        self.__ek+=effect
     def alive(self):
         if self.__sp<=-45:
-            self.__hp-150
+            self.__hp-=150
             print(f"{name} distorted.,{name} has taken 150 damage.")
-        if self.__hp<=0:
-            print(f"{name} has died.")
-            return 
+            self.__sp=0
+        elif self.__hp<=0:
+            print(f"{name} is died.")
     def status(self):
         print(f"{self.name}'s health is {self.__hp} and sanity is {self.__sp}.")
+    def battle(self):
+        print(f"{name} has to suppress a abnormality.")
+        action=(input("Fight or Flight ")).lower()
+        if action=="fight":
+            rand=random.randint(-150,-1)
+            Agent.hpeffect(Nugget,rand)  
+        elif action=="flight":
+            rand=random.randint(-45,-1)
+            Agent.speffect(Nugget,rand)
+        else:
+            print("Command not understood.")
+            Agent.hpeffect(Nugget,-100)
+            Agent.speffect(Nugget,-25)
+    def extraction(self):
+        print(f"{name} is deployed to extract enkephalin.")
+        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        if action=="zayin":
+            Agent.hpeffect(Nugget,-20)
+            Agent.ekeffect(Nugget,1)
+        elif action=="tenth":
+            Agent.hpeffect(Nugget,-40)
+            Agent.ekeffect(Nugget,2)
+        elif action=="he":
+            Agent.hpeffect(Nugget,-60)
+            Agent.ekeffect(Nugget,5)
+        elif action=="waw":
+            Agent.hpeffect(Nugget,-80)
+            Agent.ekeffect(Nugget,10)
+        elif action=="alph":
+            Agent.hpeffect(Nugget,-100)
+            Agent.ekeffect(Nugget,25)
+        else:
+            print("Not understood ALPH selected.")
+            Agent.hpeffect(Nugget,-100)
+    def rest(self):
+        print(f"{name} has a short break.")
+        Agent.hpeffect(Nugget,50)
+        Agent.speffect(Nugget,20)
+    def egogear(self):
+        print(f"{name} can now choose an E.G.O gear.")
+        action=(input("Choose a ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        if action=="zayin":
+            Agent.hpeffect(Nugget,20)
+            Agent.speffect(Nugget,-10)
+        elif action=="tenth":
+            Agent.hpeffect(Nugget,40)
+            Agent.speffect(Nugget,-20)
+        elif action=="he":
+            Agent.hpeffect(Nugget,60)
+            Agent.speffect(Nugget,-30)
+        elif action=="waw":
+            Agent.hpeffect(Nugget,80)
+            Agent.speffect(Nugget,-50)
+        elif action=="alph":
+            Agent.hpeffect(Nugget,100)
+            Agent.speffect(Nugget,-75)
+        else:
+            print("Not understood ALPH selected.")
+            Agent.hpeffect(Nugget,90)
+            Agent.speffect(Nugget,-85)
+    def spextraction(self):
+        print(f"{name} is deployed to extract enkephalin.")
+        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        if action=="zayin":
+            Agent.speffect(Nugget,-10)
+            Agent.ekeffect(Nugget,1)
+        elif action=="tenth":
+            Agent.speffect(Nugget,-15)
+            Agent.ekeffect(Nugget,2)
+        elif action=="he":
+            Agent.speffect(Nugget,-20)
+            Agent.ekeffect(Nugget,5)
+        elif action=="waw":
+            Agent.speffect(Nugget,-30)
+            Agent.ekeffect(Nugget,10)
+        elif action=="alph":
+            Agent.speffect(Nugget,-45)
+            Agent.ekeffect(Nugget,25)
+        else:
+            print("Not understood ALPH selected.")
+            Agent.speffect(Nugget,-45)
+    def score(self):
+        print(self.__ek)
     def play(self):
         while self.__hp>0:
-            event=random.randint(1,10)
+            print(f"Day {i}")
             Agent.alive(Nugget)
             Stage=(input("Continue on or pause. ")).lower()
-            if Stage=="continue":
-                event=1
+            if Stage=="continue" or Stage=="c" or Stage=="continue on":
+                i+=1
+                event=random.randint(1,5)
                 if event==1:
-                    print(f"{name} has to suppress a abnormality.")
-                    action=(input("Fight or Flight ")).lower()
-                    if action=="fight":
-                        rand=random.randint(-150,-1)
-                        Agent.hpeffect(Nugget,rand)
-                    elif action=="flight":
-                        rand=random.randint(-45,-1)
-                        Agent.speffect(Nugget,rand)
-                    else:
-                        print("Command not understood.")
-                        Agent.hpeffect(Nugget,-100)
-                        Agent.speffect(Nugget,-25)
-                if event==2:
-                    print(f"{name} is deployed to extract enkephalin.")
-                    action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
-                    if action=="zayin":
-                        Agent.hpeffect(Nugget,-20)
-                    if action=="tenth":
-                        Agent.hpeffect(Nugget,-40)
-                    if action=="he":
-                        Agent.hpeffect(Nugget,-60)
-                    if action=="waw":
-                        Agent.hpeffect(Nugget,-80)
-                    if action=="alph":
-                        Agent.hpeffect(Nugget,-100)
-                    else:
-                        print("Not understood ALPH selected.")
-                        Agent.hpeffect(Nugget,-100)
-                if event==3:
-                    print(f"{name} has a short break.")
-                    Agent.hpeffect(Nugget,50)
-                    Agent.speffect(Nugget,20)
-                if event==4:
-                    print(f"{name} can now choose an E.G.O gear.")
-                    action=(input("Choose a ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
-                    if action=="zayin":
-                        Agent.hpeffect(Nugget,20)
-                        Agent.speffect(Nugget,-10)
-                    if action=="tenth":
-                        Agent.hpeffect(Nugget,40)
-                        Agent.speffect(Nugget,-20)
-                    if action=="he":
-                        Agent.hpeffect(Nugget,60)
-                        Agent.speffect(Nugget,-30)
-                    if action=="waw":
-                        Agent.hpeffect(Nugget,80)
-                        Agent.speffect(Nugget,-50)
-                    if action=="alph":
-                        Agent.hpeffect(Nugget,100)
-                        Agent.speffect(Nugget,-75)
-                    else:
-                        print("Not understood ALPH selected.")
-                        Agent.hpeffect(Nugget,90)
-                        Agent.speffect(Nugget,-85)
-            else:
-                print("You took a quick break.")
-                Agent.status(Nugget)
-        Agent.alive(Nugget)
+                    Agent.battle(Nugget)
+                elif event==2:
+                    Agent.extraction(Nugget)
+                elif event==3:
+                    Agent.rest(Nugget)
+                elif event==4:
+                    Agent.egogear(Nugget)
+                elif event==5:
+                    Agent.spextraction(Nugget)
+                else:
+                    print("You took a quick break.")
+                    Agent.status(Nugget)
+            Agent.alive(Nugget)
+        Agent.score(Nugget)
 
 name=input("Name your agent. ")
 health=random.randint(80,150)
-Nugget=Agent(name,health,0)
+Nugget=Agent(name,health,0,0)
 Agent.play(Nugget)
