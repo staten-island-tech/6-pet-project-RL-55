72cb60ca20369728876dfa84265fc3b24f74e001
RL-55 <rickyl55@nycstudents.net>
2025-12-12 09:25:42 -0500

    Update lc.py

--- lc.py
+++ lc.py
@@ -7,24 +7,24 @@ class Agent:
         self.__sp=sanity
     def hpeffect(self,effect):
         self.__hp+=effect
-        print(f"{self.name} health has changed by {effect}.")
+        x=input(f"{self.name} health has changed by {effect}.")
     def speffect(self,effect):
         self.__sp+=effect
         if self.__sp>45:
             self.__sp=45
-        print(f"{self.name} sanity has changed by {effect}.")  
+        x=input(f"{self.name} sanity has changed by {effect}.")  
     def ekeffect(self,effect):
         self.__ek+=effect
     def corrode(self):
         if self.__sp<=-45:
             self.__hp-=150
-            print(f"{name} distorted. {name} has taken 150 damage.")
+            x=input(f"{name} distorted. {name} has taken 150 damage.")
             self.__sp=0
     def status(self):
-        print(f"{self.name}'s health is {self.__hp} and sanity is {self.__sp}.")
+        x=input(f"{self.name}'s health is {self.__hp} and sanity is {self.__sp}.")
     def battle(self):
-        print(f"{name} has to suppress a abnormality.")
-        action=(input("Fight or Flight ")).lower()
+        x=input(f"{name} has to suppress a abnormality.")
+        action=(input("Fight or Flight? ")).lower()
         if action=="fight":
             rand=random.randint(-150,-1)
             Agent.hpeffect(Nugget,rand)  
@@ -32,12 +32,12 @@ class Agent:
             rand=random.randint(-45,-1)
             Agent.speffect(Nugget,rand)
         else:
-            print("Command not understood.")
+            x=input("Command not understood.")
             Agent.hpeffect(Nugget,-100)
             Agent.speffect(Nugget,-25)
     def extraction(self):
-        print(f"{name} is deployed to extract enkephalin.")
-        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        x=input(f"{name} is deployed to extract enkephalin.")
+        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH: ")).lower()
         if action=="zayin":
             Agent.hpeffect(Nugget,-20)
             Agent.ekeffect(Nugget,1)
@@ -54,15 +54,15 @@ class Agent:
             Agent.hpeffect(Nugget,-100)
             Agent.ekeffect(Nugget,25)
         else:
-            print("Not understood ALPH selected.")
+            x=input("Not understood ALPH selected.")
             Agent.hpeffect(Nugget,-100)
     def rest(self):
-        print(f"{name} has a short break.")
+        x=input(f"{name} has a short break.")
         Agent.hpeffect(Nugget,50)
         Agent.speffect(Nugget,20)
     def egogear(self):
-        print(f"{name} can now choose an E.G.O gear.")
-        action=(input("Choose a ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        x=input(f"{name} can now choose an E.G.O gear.")
+        action=(input("Choose a ZAYIN, TENTH, HE, WAW, ALPH: ")).lower()
         if action=="zayin":
             Agent.hpeffect(Nugget,20)
             Agent.speffect(Nugget,-10)
@@ -83,8 +83,8 @@ class Agent:
             Agent.hpeffect(Nugget,90)
             Agent.speffect(Nugget,-85)
     def spextraction(self):
-        print(f"{name} is deployed to extract enkephalin.")
-        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH ")).lower()
+        x=input(f"{name} is deployed to extract enkephalin.")
+        action=(input("Work on ZAYIN, TENTH, HE, WAW, ALPH: ")).lower()
         if action=="zayin":
             Agent.speffect(Nugget,-10)
             Agent.ekeffect(Nugget,1)
@@ -101,17 +101,17 @@ class Agent:
             Agent.speffect(Nugget,-45)
             Agent.ekeffect(Nugget,25)
         else:
-            print("Not understood. ALPH selected.")
+            x=input("Not understood. ALPH selected.")
             Agent.speffect(Nugget,-45)
     def c(self):
-        print(f"{name} hear a mysterious voice.")
+        x=input(f"{name} hear a mysterious voice.")
         action=input((f"Does {name} listen or not? ")).lower
         if action=="listen":
             self.__hp-=150
-            print(f"{name} distorted.,{name} has taken 150 damage.")
+            x=input(f"{name} distorted.,{name} has taken 150 damage.")
             self.__sp=0
         elif action=="don't" or action=="dont" or action=="no" or action=="not":
-            print(f"{name} reinforced their resolve.")
+            x=input(f"{name} reinforced their resolve.")
             Agent.hpeffect(Nugget,self.__hp*2)
         else:
             print(f"{name} hides from the truth.")
@@ -122,12 +122,29 @@ class Agent:
         if action=="accept":
             rand=random.randint(1,2)
             if rand==1:
-                print(f"{name} has accepted the invitation.")
+                x=input(f"{name} has accepted the invitation.")
                 Agent.hpeffect(Nugget,-999)
             else:
-                print(f"{name} has decided not to go.")
+                x=input(f"{name} has decided not to go.")
         else:
-            print(f"{name} refused the request.")
+            x=input(f"{name} refused the request.")
+    def kcorp(self):
+        rand=random.randint(1,2)
+        if rand==1:
+            action=(input(f"{name} found a mysterious ample. What will {name} do? ")).lower
+            if action=="accept" or action=="take" or action=="pick up":
+                x=input(f"{name} injected the ample.")
+                Agent.hpeffect(Nugget,100)
+            else:
+                x=input(f"{name} ignored the ample.")
+        else:
+            action=(input(f"{name} has found a mysterious vial. What will {name} do? ")).lower
+            if action=="accept" or action=="take" or action=="pick up":
+                x=input(f"{name} injected the vial.")
+                Agent.hpeffect(Nugget,-100)
+            else:
+                x=input(f"{name} ignored the vail.")
+
     def score(self):
         print(self.__ek)
     def play(self):
@@ -139,7 +156,7 @@ class Agent:
             Stage=(input("Continue on or pause. ")).lower()
             if Stage=="continue" or Stage=="c" or Stage=="continue on":
                 day+=1
-                event=random.randint(1,37)
+                event=random.randint(1,45)
                 if 10>=event>=1:
                     Agent.battle(Nugget)
                 elif 21>=event>=11:
@@ -150,9 +167,11 @@ class Agent:
                     Agent.egogear(Nugget)
                 elif 40>=event>=31:
                     Agent.spextraction(Nugget)
-                elif event==41:
+                elif 43>=event>=41:
+                    Agent.kcorp
+                elif event==44:
                     Agent.c(Nugget)
-                elif event==42:
+                elif event==45:
                     Agent.invite(Nugget)
             else:
                 print("You took a quick break.")
